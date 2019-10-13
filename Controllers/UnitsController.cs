f8159035fcedaff5039b66e856b78bceedc081eb
meshu-dev <admin@requiredev.com>
2019-10-13 13:13:08 +0100

    Updated units and weigh ins controller

--- Controllers/UnitsController.cs
+++ Controllers/UnitsController.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using WeightTracker.Api.Models;
 using WeightTracker.Api.Repositories;
 
@@ -11,6 +11,23 @@ namespace WeightTracker.Api.Controllers
     public class UnitsController : ApiController<UnitModel>
     {
         public UnitsController(Repository<UnitModel> repository) : base(repository) { }
+
+        [HttpPost()]
+        public IActionResult Post(UnitModel model)
+        {
+            try
+            {
+                var unit = repository.Create(model);
+                if (unit == null) return BadRequest("Unit could not be created");
+
+                return Ok(unit);
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                return this.StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
 
         [HttpGet("{id:int}")]
         public IActionResult Get(int id)
@@ -42,6 +59,40 @@ namespace WeightTracker.Api.Controllers
             {
                 return this.StatusCode(StatusCodes.Status500InternalServerError, "There was an issue getting units");
             }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, UnitModel model)
+        {
+            try
+            {
+                var unit = repository.Read(id);
+                if (unit == null) return NotFound($"Unit doesn't exist with Id {id}");
+
+                model.Id = id;
+
+                unit = repository.Update(model);
+                if (unit == null) return BadRequest("Unit could not be updated");
+
+                return Ok(unit);
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                return this.StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var unit = repository.Read(id);
+            if (unit == null) return NotFound();
+
+            var isDeleted = repository.Delete(unit);
+            if (isDeleted == true) return NoContent();
+
+            return NotFound("Couldn't delete Unit");
         }
     }
 }
--- Controllers/WeighInsController.cs
+++ Controllers/WeighInsController.cs
@@ -1,16 +1,98 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using WeightTracker.Api.Models;
+using WeightTracker.Api.Repositories;
 
 namespace WeightTracker.Api.Controllers
 {
     [Route("weighins")]
     [ApiController]
-    public class WeighInsController : Controller
+    public class WeighInsController : ApiController<WeighInModel>
     {
+        public WeighInsController(Repository<WeighInModel> repository) : base(repository) { }
+
+        [HttpPost()]
+        public IActionResult Post(WeighInModel model)
+        {
+            try
+            {
+                var weighIn = repository.Create(model);
+                if (weighIn == null) return BadRequest("Weigh in could not be created");
+
+                return Ok(weighIn);
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                return this.StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                var weighIn = repository.Read(id);
+                if (weighIn == null) return NotFound($"Weigh in does not exist with Id {id}");
+
+                return Ok(weighIn);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "There was an issue getting weigh in");
+            }
+        }
+
         [HttpGet]
         public IActionResult GetAll()
         {
-            return Ok(new { Moniker = "ATL201", Name = "Atlanta Code Camp" });
+            try
+            {
+                var weighIns = repository.ReadAll();
+                if (weighIns == null) return NotFound($"No weigh ins are available");
+
+                return Ok(weighIns);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "There was an issue getting weigh ins");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, WeighInModel model)
+        {
+            try
+            {
+                var weighIn = repository.Read(id);
+                if (weighIn == null) return NotFound($"Weigh in doesn't exist with Id {id}");
+
+                model.Id = id;
+
+                weighIn = repository.Update(model);
+                if (weighIn == null) return BadRequest("Weigh in could not be updated");
+
+                return Ok(weighIn);
+            }
+            catch (Exception e)
+            {
+                //throw e;
+                return this.StatusCode(StatusCodes.Status500InternalServerError, e);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var weighIn = repository.Read(id);
+            if (weighIn == null) return NotFound();
+
+            var isDeleted = repository.Delete(weighIn);
+            if (isDeleted == true) return NoContent();
+
+            return NotFound("Couldn't delete Weigh in");
         }
     }
 }
